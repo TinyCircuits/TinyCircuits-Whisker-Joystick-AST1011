0c38a24cbf4744ad14ab7ca9d403d4fbbed04fcb
TinyCircuits <info@tinycircuits.com>
2020-01-02 13:44:45 -0500

    updated library and example code

--- libraries/WirelingInputs/WirelingInputs.cpp
+++ libraries/WirelingInputs/WirelingInputs.cpp
@@ -1,14 +1,14 @@
-#include "TinyJoystick.h"
+#include "WirelingInputs.h"
 
 #include <inttypes.h>
 #include "Arduino.h"
 #include <Wire.h>
 
-TinyJoystick::TinyJoystick() {
+SH7010::SH7010() {
   // default constructor
 }
 
-int TinyJoystick::begin() {
+int SH7010::begin() {
   //Detect address
   I2Caddress = SX1505_I2CADDR;
   Wire.beginTransmission(I2Caddress);
@@ -24,7 +24,7 @@ int TinyJoystick::begin() {
 }
 
 // writes byte "regData" to register "regAddr"
-void TinyJoystick::write(uint8_t regAddr, uint8_t regData) {
+void SH7010::write(uint8_t regAddr, uint8_t regData) {
   Wire.beginTransmission(SX1505_I2CADDR);
   Wire.write(regAddr);
   Wire.write(regData);
@@ -32,7 +32,7 @@ void TinyJoystick::write(uint8_t regAddr, uint8_t regData) {
 }
 
 // returns byte stored within specified register
-uint8_t TinyJoystick::read(uint8_t regAddr) {
+uint8_t SH7010::read(uint8_t regAddr) {
   Wire.beginTransmission(SX1505_I2CADDR);
   Wire.write(regAddr);
   debug = Wire.endTransmission(); // debug set to value returned (error checking)
@@ -44,7 +44,7 @@ uint8_t TinyJoystick::read(uint8_t regAddr) {
 }
 
 // initialize SX1505 for use with SH7010
-int TinyJoystick::init(void) {
+int SH7010::init(void) {
   write(SX1505_REGDIR, 0xFF);           // set I/O to all input
     delay(5);
   write(SX1505_REG_PLD_MODE, 0x00);     // disable PLD input (default)
@@ -57,14 +57,27 @@ int TinyJoystick::init(void) {
 }
 
 // return data from data register
-uint8_t TinyJoystick::getRegData(void) {
+uint8_t SH7010::getRegData(void) {
   value = 0x00;
   value = read(SX1505_REG_DATA);
   return value;
 }
 
+/*TinyJoystick*/
+
+TinyJoystick::TinyJoystick() {
+  // default constructor
+}
+
+
 // return joystick position
 void TinyJoystick::getPosition(void) {
+  // reset direction variables before setting and sending new values
+  up = 0;
+  down = 0;
+  left = 0;
+  right = 0;
+
   getRegData();
 
   uint8_t dirtyValue = ~value;  // flip bits since active low
@@ -87,4 +100,23 @@ void TinyJoystick::getPosition(void) {
     left = 0;
     right = 0;
   }
+}
+
+TinyRotary::TinyRotary() {
+  // default constructor
+}
+
+// return rotary position
+uint8_t TinyRotary::getPosition(void) {
+  getRegData();
+
+  uint8_t cleanValue = 0x00;                // initialize all 0
+  uint8_t dirtyData = ~value;               // flip bits since active low
+  
+  cleanValue |= (dirtyData & 0x08);         // check bit 3, no shift
+  cleanValue |= ((dirtyData & 0x02) << 1);  // check bit 1, shift to bit 2
+  cleanValue |= ((dirtyData & 0x10) >> 3);  // check bit 4, shift to bit 1
+  cleanValue |= (dirtyData & 0x01);         // check bit 0, no shift
+
+  return cleanValue;
 }
